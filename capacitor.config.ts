5544dc74f2b104e59c2922133f4d26c6789d13df
Jefferson Pozo <misaelpozocatuto@gmail.com>
2025-11-20 23:04:45 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IOT',
+  webDir: 'www'
+};
+
+export default config;
