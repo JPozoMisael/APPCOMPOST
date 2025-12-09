e80a19bffef4cfe3777ae78770c02b695f8407de
e_d_d <emiliodelgadoladines2014@gmail.com>
2025-12-09 00:16:51 -0500

    cambios para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://TU-DOMINIO.com/api',
+  apiBaseUrl: 'https://apicompostera.servidorbienmaquiavelicoypoderoso.xyz/api',
   adminApiKey: 'TU_SUPER_CLAVE_LARGA_64+CARACTERES', // en prod usa otra clave
 };
